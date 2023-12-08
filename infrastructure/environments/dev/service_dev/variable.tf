8c9dc423db998789f2dbcad38438da47770d9800
Abhinav Bisht <bisht.abhinav007@gmail.com>
2023-12-08 23:43:09 +0530

    .

--- infrastructure/environments/dev/service_dev/variable.tf
+++ infrastructure/environments/dev/service_dev/variable.tf
@@ -1,2 +1,6 @@
-variable "ss_resource_group_name" {}
-variable "keyvault_name" {}
+variable "ss_resource_group_name" {
+    default = "shared_services"
+}
+variable "keyvault_name" {
+    default = "keyvault-dev"
+}
